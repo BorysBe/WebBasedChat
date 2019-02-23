9c289b430e4bd98375b7d6e2b801b2a136c0221b
BorysBe <b.bobulski@wp.pl>
2019-02-23 14:23:53 +0100

    create specflow project with first user story scenario

---
+++ WebBasedChat/WebBasedChat.Specification/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebBasedChat.Specification")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebBasedChat.Specification")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("47fff86b-9b57-4639-81f0-684d061860ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebBasedChat/WebBasedChat.Specification/WebChat.feature
@@ -0,0 +1,10 @@
+﻿Feature: WebChat
+	In order to set up communication
+	As a wpf application user
+	I want to chat with other user
+
+@mytag
+Scenario: Start application
+	Given User run application
+	When User see first screen
+	Then Should see Screen 1
