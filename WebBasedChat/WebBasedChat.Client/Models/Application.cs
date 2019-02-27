b9b37a5f86b68851cfaff7679557fdd1c091d626
BorysBe <b.bobulski@wp.pl>
2019-02-27 10:58:23 +0100

    command factory usage cleanup

--- WebBasedChat/WebBasedChat.Client/Models/Application.cs
+++ WebBasedChat/WebBasedChat.Client/Models/Application.cs
@@ -1,24 +1,20 @@
 ﻿using System;
-using WebBasedChat.Client.Factories.Contracts;
 
 namespace WebBasedChat.Client.Models
 {
     public class Application : IDisposable
     {
-        public Application(State state, ICommandFactory commandFactory)
+        private State State { get; }
+
+        public Application(State state)
         {
             State = state;
-            _commandFactory = commandFactory;
         }
 
         public void Dispose()
         {
             // clean up
         }
-
-        private readonly ICommandFactory _commandFactory;
-
-        private State State { get; }
 
         public void Enter(string message)
         {
